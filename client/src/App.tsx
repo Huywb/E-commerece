bf7471dc69172a1439e33d215bcaaa988ed04e19
Huywb <pthhuy1306@gmail.com>
2024-09-03 15:09:32 +0700

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
+
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/collection" element={<Collection></Collection>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Home></Home>}></Route>
+        <Route path="/product/:productId" element={<Product></Product>}></Route>
+        <Route path="/cart" element={<Cart></Cart>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/place-order" element={<PlaceOrder></PlaceOrder>}></Route>
+        <Route path="/orders" element={<Orders></Orders>}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
